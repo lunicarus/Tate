2ecfa6178d562ec441be8f6feeaf7b24576a1e68
lucas da silva cruz <lucas6jau@gmail.com>
2023-09-23 22:59:04 -0300

    feat: Create 'CartesianPlane' class

---
+++ untitled/src/main/java/org/example/CartesianPlane.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+class CartesianPlane {
+    private int X;
+    private int Y;
+
+
+    public CartesianPlane(int x, int y) {
+        X = x;
+        Y = y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+}
