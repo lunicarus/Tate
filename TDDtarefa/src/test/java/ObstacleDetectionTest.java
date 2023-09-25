1ce1dff0eaac4cc1456d7f538679f913012f66f6
lucas da silva cruz <lucas6jau@gmail.com>
2023-09-25 13:49:07 -0300

    test: add test for beam values below -90

--- TDDtarefa/src/test/java/ObstacleDetectionTest.java
+++ TDDtarefa/src/test/java/ObstacleDetectionTest.java
@@ -16,4 +16,29 @@ class ObstacleDetectionTest {
     Bot testRobot = new Bot(x,y,angle,beam,distance);
         assertThat(testRobot.findObstacle()).isEqualTo("(0,5)");
     }
+    @Test
+    @DisplayName("beam is greater than 90")
+    void beamGreaterThan90Test(){
+        int x = 0;
+        int y = 0;
+        int angle = 45;
+        int beam = 91;
+        int distance = 5;
+        Bot testRobot = new Bot(x,y,angle,beam,distance);
+        assertThat(testRobot.findObstacle()).isEqualTo("Erro");
+    
+    }
+
+    @Test
+    @DisplayName("Beam is lower than -90")
+    void beamLowerThan90Test(){
+        int x = 0;
+        int y = 0;
+        int angle = 45;
+        int beam = -91;
+        int distance = 5;
+        Bot testRobot = new Bot(x,y,angle,beam,distance);
+        assertThat(testRobot.findObstacle()).isEqualTo("Erro");
+
+    }
 }
