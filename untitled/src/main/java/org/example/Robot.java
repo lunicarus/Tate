88ad2d92de1336456771364ccd40d9af00f6f609
lucas da silva cruz <lucas6jau@gmail.com>
2023-09-24 21:53:42 -0300

    fix: correct constructor and formulas

--- untitled/src/main/java/org/example/Robot.java
+++ untitled/src/main/java/org/example/Robot.java
@@ -1,40 +1,31 @@
 package org.example;
 
-import java.util.Objects;
-
 class Robot {
     private int x;
     private int y;
     private int beam ;
     private int distance;
-    private int angle = 90;
+    private int angle;
 
-    public Robot(int x, int y, int beam, int distance) {
+    public Robot(int x, int y,int angle ,int beam,int distance) {
         this.x = x;
         this.y = y;
-        this.beam = beam;
-        this.distance = distance;
-    }
-    public Robot(int x, int y, int beam, int distance,int angle) {
-        this.x = x;
-        this.y = y;
-        this.beam = beam;
+        this.beam = beam-90;
         this.distance = distance;
         this.angle = angle;
     }
 
     public String findObstacle(){
-        double Radians = Math.toRadians(angle - 90);
-        if (beam < 0 || beam > 180 || distance < 0 || distance > 800) {
+        if (beam < -90 || beam > 90 || distance < 0) {
             return "Erro";}
-        else {
-            this.x += (int) (distance * Math.cos(Radians));
-            this.y += (int) (distance * Math.sin(Radians));
-            StringBuilder cordenates = new StringBuilder("(");
-            cordenates.append(x);cordenates.append(",");
-            cordenates.append(y);cordenates.append(")");
-            return cordenates.toString();
-        }
+        double polarCoordinate =  distance;
+        double Radians = Math.toRadians(angle + beam);
+            int obstacleX = (int) Math.round(this.x +(polarCoordinate * Math.cos(Radians)));
+            int obstacleY = (int) Math.round(this.y + (polarCoordinate * Math.sin(Radians)));
+            StringBuilder coordinates = new StringBuilder();
+            coordinates.append("(").append(obstacleX).append(",").append(obstacleY).append(")");
+            return coordinates.toString();
+
     }
 
 }
