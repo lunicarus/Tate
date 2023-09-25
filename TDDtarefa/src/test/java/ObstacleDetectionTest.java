83fee1a5212d4f01b892816e4945673a963ead2b
lucas da silva cruz <lucas6jau@gmail.com>
2023-09-25 15:03:33 -0300

    refactor: optimize beam interval testing

--- TDDtarefa/src/test/java/ObstacleDetectionTest.java
+++ TDDtarefa/src/test/java/ObstacleDetectionTest.java
@@ -4,41 +4,16 @@ import static org.assertj.core.api.Assertions.*;
 
 
 class ObstacleDetectionTest {
-
     @Test
-    @DisplayName("Should Return (0,5)")
-    void ShouldReturn05(){
-    int x = 0;
-    int y = 0;
-    int angle = 45;
-    int beam = 45;
-    int distance = 5;
-    Bot testRobot = new Bot(x,y,angle,beam,distance);
-        assertThat(testRobot.findObstacle()).isEqualTo("(0,5)");
-    }
-    @Test
-    @DisplayName("Beam is greater than 90")
-    void beamGreaterThan90Test(){
-        int x = 0;
-        int y = 0;
+    @DisplayName("Bot Beam is between -90 and 90")
+    void beamIntervalIsMinus90And90(){
+        int x = -4;
+        int y = 4;
         int angle = 45;
-        int beam = 181;
-        int distance = 5;
+        int beam = 135;
+        int distance = -1;
         Bot testRobot = new Bot(x,y,angle,beam,distance);
-        assertThat(testRobot.findObstacle()).isEqualTo("Erro");
-    
-    }
-
-    @Test
-    @DisplayName("Beam is lower than -90")
-    void beamLowerThan90Test(){
-        int x = 0;
-        int y = 0;
-        int angle = 45;
-        int beam = -91;
-        int distance = 5;
-        Bot testRobot = new Bot(x,y,angle,beam,distance);
-        assertThat(testRobot.findObstacle()).isEqualTo("Erro");
+        assertThat(testRobot.getBeam()).isBetween(-90,90);
 
     }
     @Test
