44a90ea32fe562881f49002fdb07378bf4ebfd64
lucas da silva cruz <lucas6jau@gmail.com>
2023-09-25 13:49:38 -0300

    fix: sanitize beam values

--- TDDtarefa/src/main/java/Bot.java
+++ TDDtarefa/src/main/java/Bot.java
@@ -3,10 +3,15 @@ public class Bot {
     int beam;
     int distance;
     public Bot(int x, int y, int angle, int beam, int distance) {
+        this.angle = angle;
+        this.beam = beam;
+        this.distance = distance;
 
     }
 
     public String findObstacle() {
+        if (beam < -90 || beam > 90) {
+            return "Erro";}
         return "(0,5)";
     }
 }
