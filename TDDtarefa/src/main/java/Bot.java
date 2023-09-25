fae69e4717afdfb7663fcd50b48a839fc5cb37ee
lucas da silva cruz <lucas6jau@gmail.com>
2023-09-25 13:35:10 -0300

    feat: create bot class and findObstacle method

---
+++ TDDtarefa/src/main/java/Bot.java
@@ -0,0 +1,12 @@
+public class Bot {
+    int angle;
+    int beam;
+    int distance;
+    public Bot(int x, int y, int angle, int beam, int distance) {
+
+    }
+
+    public String findObstacle() {
+        return "(0,5)";
+    }
+}
